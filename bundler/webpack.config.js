8f566bf28300dbbaca04874768644afc2431ac56
Julio Salas <jscomic@gmail.com>
2022-07-17 19:33:50 -0500

    bundler exercise and time tracking

---
+++ bundler/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  devServer: {
+    open: true,
+    hot: true,
+    port: 9000,
+  },
+};
